6b883e5b9abfa790494fa6480e0e1ea62b363468
leyafo <leyafo@gmail.com>
2023-08-10 01:04:35 +0800

    add search option click event

--- main/preload.js
+++ main/preload.js
@@ -14,6 +14,7 @@ contextBridge.exposeInMainWorld('utils', {
     "getCardsByTag",
     "getTrashCards",
     "getNoTagCards",
+    "getCardsByMiddleID",
   ]
   const otherDBFunctions = [
     "createNewCard", 
