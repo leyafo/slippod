a9c9a5226cc813e7534bd43cce632dfda5841748
leyafo <leyafo@gmail.com>
2023-08-27 15:24:55 +0800

    add references completed hints

--- resource/app/autocomplete.js
+++ resource/app/autocomplete.js
@@ -1,33 +1,143 @@
-function showCustomMenu(cm) {
-    var cursor = cm.getCursor(); // Get the current cursor position
-    var coords = cm.cursorCoords(cursor, 'window'); // Get the coordinates of the cursor
+import * as CM from "./common"
+const menuTemplate = document.getElementById("completeReferenceMenu")
 
-    var menu = document.createElement('div');
-    menu.style.position = 'absolute';
-    menu.style.left = coords.left + 'px';
-    menu.style.top = coords.bottom + 'px';
-    menu.className = 'custom-menu';
-    
-    // Populate menu with your custom HTML
-    menu.innerHTML = '<div>Your HTML here</div>';
-
-    // Attach event handlers, etc. as needed for the menu items
-
-    document.body.appendChild(menu);
-
-    // Close the menu if clicked outside
-    document.addEventListener('click', function closeMenu(event) {
-        if (!menu.contains(event.target)) {
-            menu.remove();
-            document.removeEventListener('click', closeMenu);
-        }
+function createCardSuggestion(card){
+    const div = document.createElement("div");
+    div.textContent = card.entry.trim();
+    div.dataset.id = card.id; 
+    div.onclick = function(event){
+        // let cursor = cm.getCursor(), token = cm.getTokenAt(cursor);
+    };
+    div.addEventListener("mouseover", function () {
     });
+    return div
+}
+
+function showCustomMenu(cm, editor) {
+    const coordsPos = cm.cursorCoords(true, "page");
+
+    var menu = document.createElement("div");
+    menu.style.position = "absolute"; // Make sure the position is set to 'absolute'
+    menu.style.left = `${coordsPos.left}px`;
+    menu.style.top = `${coordsPos.bottom}px`;
+    menu.classList.add("completeAtMenu")
+
+    db.getCards(0, CM.limitItems).then(function (cards) {
+        for (let card of cards) {
+            const div = createCardSuggestion(card);
+            menu.appendChild(div);
+        }
+        menu.firstChild.classList.add("highlighted");
+        document.body.appendChild(menu);
+        editor.on("keydown", completeAtKeyHandler(editor, menu, cm))
+    });
+}
+function completeAtKeyHandler(editor, menu, cm){
+    let inputText = "";
+    let handler = function(cm, event){
+        switch (event.keyCode) {
+            case 38: // Arrow Up
+                navigateMenu(event, menu);
+                event.preventDefault(); // Prevent scrolling
+                break;
+            case 40: // Arrow Down
+                navigateMenu(event, menu);
+                event.preventDefault(); // Prevent scrolling
+                break;
+            case 13://Enter key
+                event.preventDefault(); // Prevent default action
+                const item = menu.querySelector('.highlighted'); // Assume you've highlighted the selected item
+                if (item) {
+                    const cursor = editor.getCursor(); // Get the current cursor position
+                    const line = editor.getLine(cursor.line); // Get the content of the current line
+                    const token = cm.getTokenAt(cursor);
+                    inputText = token.string.slice(1)
+                    console.log(token, cursor.line, cursor.ch);
+                    editor.replaceRange("", { line: cursor.line, ch: 0 }, { line: cursor.line, ch: cursor.ch }); // Replace line content
+                    editor.replaceRange(`[${token.string}](/links/${item.dataset.id})\n`, cursor); // Insert new text
+                    closeAtMenu(menu);
+                    editor.off('keydown', handler);
+                }
+            case 32:
+            case 27:
+            case 51:
+                {
+                    closeAtMenu(menu);
+                    editor.off('keydown', handler);
+                }
+            default: //refresh the sugestion list
+                {
+                    const cursor = editor.getCursor(); // Get the current cursor position
+                    const token = cm.getTokenAt(cursor);
+                    if(inputText != token.string.slice(1)){
+                        inputText = token.string.slice(1);
+                        if (inputText.length > 0){
+                            db.searchCards(inputText, 0, 100).then(function(cards){
+                                menu.innerHTML = ""
+                                for (let card of cards) {
+                                    const div = createCardSuggestion(card);
+                                    menu.appendChild(div);
+                                }
+                                if (cards.length == 0){
+                                    closeAtMenu(menu);
+                                    editor.off('keydown', handler);
+                                }
+                            });
+                        }
+                    }
+                }
+        }
+    }
+
+    return handler
+}
+
+function closeAtMenu(menu){
+    if(menu != null && menu.parentNode != null){
+        menu.parentNode.removeChild(menu)
+    }
+}
+
+// Navigation function
+function navigateMenu(event, menu) {
+    console.log('test');
+    const items = Array.from(menu.querySelectorAll('div'));  // Replace 'div' with the actual item selector if different
+    const highlighted = menu.querySelector('.highlighted');
+    let index = items.indexOf(highlighted);
+
+    // Up Arrow
+    if (event.key === 'ArrowUp') {
+        event.preventDefault();  // Prevent default to stop any side behavior (like scrolling)
+        if (index > 0) {
+            index--;
+        } else {
+            index = items.length - 1;  // Cycle to the bottom
+        }
+    }
+    // Down Arrow
+    else if (event.key === 'ArrowDown') {
+        event.preventDefault();
+        if (index < items.length - 1) {
+            index++;
+        } else {
+            index = 0;  // Cycle to the top
+        }
+    }
+    // Not an arrow key
+    else {
+        return;
+    }
+
+    // Remove old highlight
+    if (highlighted) {
+        highlighted.classList.remove('highlighted');
+    }
+
+    // Add new highlight
+    items[index].classList.add('highlighted');
 }
 
 function autocompleteHints(cm, option) {
-  cm.on("shown", function () {
-    console.log("hhhhkdjfls");
-  });
   return new Promise(function (accept) {
     let cursor = cm.getCursor(),
       lineContent = cm.getLine(cursor.line),
@@ -39,7 +149,7 @@ function autocompleteHints(cm, option) {
         let hints = [];
         for (let t of tags) {
           hints.push({
-            text: `[${t.tag}](/tag/${t.tag})`,
+            text: `[${t.tag}](/tag/${t.tag}) `,
             displayText: t.tag,
           });
         }
@@ -75,4 +185,5 @@ CodeMirror.defineMode("hashtags", function (config, parserConfig) {
 
 export {
     autocompleteHints,
+    showCustomMenu,
 }
--- resource/app/keys.js
+++ resource/app/keys.js
@@ -1,9 +1,13 @@
-import * as CM from "./common.js"
-import * as UI from "./ui.js"
+import * as CM from "./common.js";
+import * as UI from "./ui.js";
 
 //全局快捷键盘
-document.addEventListener('keydown', function(event) {
-    if (event.key == "k" &&event.ctrlKey){
+document.addEventListener("keydown", function (event) {
+    if (event.key == "k" && event.ctrlKey) {
+        //check the editing status
+        if (CM.cardsList.querySelector("[data-editing='true']") !== null){
+            return
+        }
         CM.showOmniSearchAndFocus();
         CM.searchBox.focus();
         event.preventDefault();
@@ -12,26 +16,26 @@ document.addEventListener('keydown', function(event) {
 
 //搜索框快捷键
 CM.searchBox.addEventListener("keydown", function (event) {
-  if (event.key === "ArrowDown") {
-    UI.highlightNote(event, CM.highlightDown);
-  } else if (event.key === "ArrowUp") {
-    UI.highlightNote(event, CM.highlightUp);
-  } else if (event.key === "Enter") {
-    if (event.ctrlKey) {
-      event.stopPropagation();
-      return;
-    }
+    if (event.key === "ArrowDown") {
+        UI.highlightNote(event, CM.highlightDown);
+    } else if (event.key === "ArrowUp") {
+        UI.highlightNote(event, CM.highlightUp);
+    } else if (event.key === "Enter") {
+        if (event.ctrlKey) {
+            event.stopPropagation();
+            return;
+        }
 
-    const highlightedSuggestion = document.querySelector(
-      "#suggestionResults .highlighted"
-    );
-    if (highlightedSuggestion) {
-        console.log(highlightedSuggestion.dataset.id);
-        UI.handleOptionSelect(highlightedSuggestion.dataset.id);
-        UI.clearSearch(event);
-        event.stopPropagation();
-    }
-  }else if (event.key == "n" && event.ctrlKey){
+        const highlightedSuggestion = document.querySelector(
+            "#suggestionResults .highlighted"
+        );
+        if (highlightedSuggestion) {
+            console.log(highlightedSuggestion.dataset.id);
+            UI.handleOptionSelect(highlightedSuggestion.dataset.id);
+            UI.clearSearch(event);
+            event.stopPropagation();
+        }
+    } else if (event.key == "n" && event.ctrlKey) {
         const searchTerm = CM.searchBox.value;
         db.createNewCard(searchTerm).then((newCardID) => {
             db.getCardByID(newCardID).then((card) => {
@@ -42,7 +46,7 @@ CM.searchBox.addEventListener("keydown", function (event) {
         });
         UI.clearSearch(event);
         event.stopPropagation();
-  } else if (event.key == "Escape") {
+    } else if (event.key == "Escape") {
         UI.clearSearch(event);
-  }
+    }
 });
