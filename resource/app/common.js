022057e091d7fb06df2ce787e9e2bd31b6ab3878
leyafo <leyafo@gmail.com>
2023-08-31 17:07:15 +0800

    control panel

--- resource/app/common.js
+++ resource/app/common.js
@@ -84,3 +84,35 @@ export function unHighlightItem(highlightedClass, parentElement){
         item.classList.remove(highlightedClass)
     }
 }
+
+export function timeAgo(unixTimestamp) {
+    const currentTime = Math.floor(Date.now() / 1000); // Convert to Unix timestamp
+    const timeDifference = currentTime - unixTimestamp;
+
+    if (timeDifference < 60) {
+        return `${timeDifference} seconds ago`;
+    }
+
+    const minutes = Math.floor(timeDifference / 60);
+    if (minutes < 60) {
+        return `${minutes} minutes ago`;
+    }
+
+    const hours = Math.floor(minutes / 60);
+    if (hours < 24) {
+        return `${hours} hours ago`;
+    }
+
+    const days = Math.floor(hours / 24);
+    if (days < 30) {
+        return `${days} days ago`;
+    }
+
+    const months = Math.floor(days / 30);
+    if (months < 12) {
+        return `${months} months ago`;
+    }
+
+    const years = Math.floor(days / 365);
+    return `${years} years ago`;
+}
