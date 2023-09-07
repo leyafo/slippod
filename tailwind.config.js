69194eeb25589def437c3be7498f1151059e3f93
Sergey L <ilizc.com@gmail.com>
2023-09-08 00:11:04 +0800

    added basic css style

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["./resource/**/*.{css,html,js}"],
   theme: {
+    fontSize: {
+      sm: '0.86666667rem',
+      base: '0.93333333rem',
+      xl: '1rem',
+      '2xl': '1.53333333rem',
+    },
     extend: {},
   },
   plugins: [],
