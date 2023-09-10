df84cf041dec03e830019282692f69d0d1cfb250
leyafo <leyafo@gmail.com>
2023-09-10 22:13:41 +0800

    bug fix

--- main/main.js
+++ main/main.js
@@ -22,7 +22,7 @@ app.whenReady().then(() => {
         // On macOS it's common to re-create a window in the app when the
         // dock icon is clicked and there are no other windows open.
         if (BrowserWindow.getAllWindows().length === 0) {
-            mainWindow = windowMgr.createMainWindow;
+            mainWindow = windowMgr.createMainWindow();
         }
     });
 });
