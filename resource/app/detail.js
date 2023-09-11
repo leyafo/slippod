802fd3d0541b68d653a6eba3e38c9711d10aeed0
Sergey L <ilizc.com@gmail.com>
2023-09-11 23:22:32 +0800

    detailwindow css style with js and html changes

--- resource/app/detail.js
+++ resource/app/detail.js
@@ -2,36 +2,46 @@
 import {linkClick, fillingCardItem } from "./common";
 
 const cardTemplate = document.getElementById('cardTemplate')
+const mainCardArea = document.getElementById('mainCardArea')
+const linkedCards = document.getElementById('linkedCards')
+const linkedCardsCount = document.getElementById('linkedCardsCount')
+const headerBarTitle = document.getElementById('headerBarTitle')
 
 document.addEventListener('click', linkClick) 
 
-function displayCard(card){
+function displayCard(card, parentElement) {
     const cardItem = cardTemplate.content.cloneNode(true); 
     fillingCardItem(cardItem, card);
     
     // Append the cardItem fragment to the body
-    document.body.appendChild(cardItem);
+    parentElement.appendChild(cardItem);
 
     // Return the actual displayed node (assuming it's the last child of the body after appending)
-    return document.body.lastElementChild;
+    return parentElement.lastElementChild;
 }
 
 window.addEventListener('DOMContentLoaded', function() {
     const urlParams = new URLSearchParams(window.location.search);
     const id = urlParams.get('cardID');
-    db.getCardDetails(id).then(function(cardDetail){
+
+    headerBarTitle.textContent = `Note @${id} Details`;
+
+    db.getCardDetails(id).then(function(cardDetail) {
         window.cardDetail = cardDetail;
-        const cardItem = displayCard(cardDetail.card)
-        if (cardDetail.references.length > 0){
+        const cardItem = displayCard(cardDetail.card, mainCardArea);
+        if (cardDetail.references.length > 0) {
             const metaInfo = cardItem.querySelector('.itemMetaInfo')
             let span = document.createElement('span');
-            span.textContent= `${cardDetail.references.length} Linked Note`
-            span.className
+
+            let noteText = cardDetail.references.length === 1 ? 'Linked Note' : 'Linked Notes';
+            span.textContent = ` · ${cardDetail.references.length} ${noteText}`;
+            linkedCardsCount.textContent = `${cardDetail.references.length} ${noteText}`;
+
             metaInfo.appendChild(span);
         }
         // document.body.appendChild(cardItem);
-        for(let refCard of cardDetail.references){
-            displayCard(refCard)
+        for(let refCard of cardDetail.references) {
+            displayCard(refCard, linkedCards);
         }
     })
 })
