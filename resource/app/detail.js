d547139884b6d5c3ca4f10316ede326c5858c90e
Sergey L <ilizc.com@gmail.com>
2023-09-12 17:34:13 +0800

    added backlinks and outgoing links to card detail page

--- resource/app/detail.js
+++ resource/app/detail.js
@@ -1,17 +1,20 @@
-
-import {linkClick, fillingCardItem } from "./common";
+import * as CM from './common.js';
 
 const cardTemplate = document.getElementById('cardTemplate')
 const mainCardArea = document.getElementById('mainCardArea')
 const linkedCards = document.getElementById('linkedCards')
-const linkedCardsCount = document.getElementById('linkedCardsCount')
+const backlinksCards = document.getElementById('backlinksCards')
+const outgoingLinksCards = document.getElementById('outgoingLinksCards')
+const linkedCardsTab = document.getElementById('linkedCardsTab')
+const backlinkTab = document.getElementById('backlinkTab')
+const outgoingLinkTab = document.getElementById('outgoingLinkTab')
 const headerBarTitle = document.getElementById('headerBarTitle')
 
-document.addEventListener('click', linkClick) 
+document.addEventListener('click', CM.linkClick) 
 
 function displayCard(card, parentElement) {
     const cardItem = cardTemplate.content.cloneNode(true); 
-    fillingCardItem(cardItem, card);
+    CM.fillingCardItem(cardItem, card);
     
     // Append the cardItem fragment to the body
     parentElement.appendChild(cardItem);
@@ -29,19 +32,56 @@ window.addEventListener('DOMContentLoaded', function() {
     db.getCardDetails(id).then(function(cardDetail) {
         window.cardDetail = cardDetail;
         const cardItem = displayCard(cardDetail.card, mainCardArea);
-        if (cardDetail.references.length > 0) {
-            const metaInfo = cardItem.querySelector('.itemMetaInfo')
-            let span = document.createElement('span');
 
-            let noteText = cardDetail.references.length === 1 ? 'Linked Note' : 'Linked Notes';
-            span.textContent = ` · ${cardDetail.references.length} ${noteText}`;
-            linkedCardsCount.textContent = `${cardDetail.references.length} ${noteText}`;
+        if (cardItem) {
+            let backlinkTabLabel = backlinkTab.querySelector('.label');
+            backlinkTabLabel.textContent = `Backlinks(${cardDetail.referencesBy.length})`;
 
-            metaInfo.appendChild(span);
+            // select backlinkTab by default
+            backlinkTab.classList.add('selected');
+
+            let outgoingLinkTabLabel = outgoingLinkTab.querySelector('.label');
+            outgoingLinkTabLabel.textContent = `Outgoinglinks(${cardDetail.references.length})`;
+
+            CM.toggleElementShown(linkedCardsTab);
         }
-        // document.body.appendChild(cardItem);
-        for(let refCard of cardDetail.references) {
-            displayCard(refCard, linkedCards);
+        
+        if (cardDetail.referencesBy.length > 0) {
+            for(let refCard of cardDetail.referencesBy) {
+                // displayCard(refCard, linkedCards);
+                displayCard(refCard, backlinksCards);
+            }
+        } else {
+            let noBacklinkMessage = document.createElement('div');
+            noBacklinkMessage.classList.add('noLinkMessage');
+            noBacklinkMessage.textContent = "No backlinks";
+            backlinksCards.appendChild(noBacklinkMessage);
+        }
+        if (cardDetail.references.length > 0) {
+            for(let refCard of cardDetail.references) {
+                displayCard(refCard, outgoingLinksCards);
+            }
+        } else {
+            let noOutgoingLinkMessage = document.createElement('div');
+            noOutgoingLinkMessage.classList.add('noLinkMessage');
+            noOutgoingLinkMessage.textContent = "No outgoing links";
+            outgoingLinksCards.appendChild(noOutgoingLinkMessage);
         }
     })
 })
+
+CM.clickHandle("#backlinkTab", function() {
+    backlinkTab.classList.add('selected');
+    outgoingLinkTab.classList.remove('selected');
+
+    backlinksCards.classList.remove('hidden');
+    outgoingLinksCards.classList.add('hidden');
+});
+
+CM.clickHandle("#outgoingLinkTab", function() {
+    outgoingLinkTab.classList.add('selected');
+    backlinkTab.classList.remove('selected');
+
+    outgoingLinksCards.classList.remove('hidden');
+    backlinksCards.classList.add('hidden');
+});
