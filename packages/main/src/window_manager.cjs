b3297f0a6192f310a4e7b75576f4fe4c31311236
leyafo <leyafo@gmail.com>
2023-09-25 17:40:42 +0800

    add new packing configurations

---
+++ packages/main/src/window_manager.cjs
@@ -0,0 +1,158 @@
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+
+console.log(import.meta.env.DEV);
+class WindowManager{
+    constructor(){
+        this.mainWindow = null
+        this.settingsWindow = null
+    }
+
+    getMainWindow(){
+        return this.mainWindow;
+    }
+
+    getSettingsWindow(){
+        if(this.settingsWindow == null){
+            this.settingsWindow = this.createSettingsWindow();
+        }
+        return this.settingsWindow;
+    }
+
+    #loadEntryPoint(window, entryPointHTML){
+        if (import.meta.env.DEV && import.meta.env.VITE_DEV_SERVER_URL !== undefined) {
+            /**
+             * Load from the Vite dev server for development.
+             * 这里要小心，如果自己组装url时，tailwindow会无法正确加载，但html又可以。
+             */
+            const urlPath = new URL(
+                entryPointHTML,
+                import.meta.env.VITE_DEV_SERVER_URL).toString();
+            window.loadURL(urlPath);
+          } else {
+            /**
+             * Load from the local file system for production and test.
+             *
+             * Use BrowserWindow.loadFile() instead of BrowserWindow.loadURL() for WhatWG URL API limitations
+             * when path contains special characters like `#`.
+             * Let electron handle the path quirks.
+             * @see https://github.com/nodejs/node/issues/12682
+             * @see https://github.com/electron/electron/issues/6869
+             */
+            window.loadFile(path.resolve(__dirname, '../../renderer/dist/', entryPointHTML));
+          }
+    }
+
+    createMainWindow() {
+        let windowConfig = {
+            width: 800,
+            height: 600,
+            minWidth: 400,
+            minHeight: 400,
+            titleBarStyle: "hidden",
+            webPreferences: {
+                preload: path.join(app.getAppPath(), 'packages/preload/dist/main.cjs'),
+                scrollBounce: true
+            },
+        };
+    
+        let activeWindow = BrowserWindow.getFocusedWindow();
+        if (activeWindow) {
+            let { x, y } = activeWindow.getBounds();
+            windowConfig.x = x + 50;
+            windowConfig.y = y + 50;
+        }
+
+        this.mainWindow = new BrowserWindow(windowConfig);
+        this.#loadEntryPoint(this.mainWindow, 'index.html');
+    
+        if (import.meta.env.DEV) {
+            const contextMenu = require("electron-context-menu");
+            contextMenu({
+                prepend: (defaultActions, params, browserWindow) => [
+                    { type: "separator" },
+                ],
+            });
+        }        
+        this.mainWindow.once("ready-to-show", () => {
+            this.mainWindow.show();
+        });
+    
+        return this.mainWindow;
+    }    
+
+    createSettingsWindow() {
+        if (this.mainWindow === null) {
+            throw new Error("Main window must be initialized before settings window");
+        }
+
+        this.settingsWindow = new BrowserWindow({
+            width: 400,
+            height: 400,
+            minWidth: 400,
+            minHeight: 400,
+            webPreferences: {
+                preload: path.join(app.getAppPath(), 'packages/preload/dist/settings.cjs'),
+                scrollBounce: true
+            },
+            parent: this.mainWindow,
+            modal: true,
+            show: true,
+        });
+        this.#loadEntryPoint(this.settingsWindow, 'setting.html');
+
+        this.settingsWindow.on("closed", () => {
+            this.settingsWindow = null;
+        });
+        this.settingsWindow.setMenuBarVisibility(false);
+
+        return this.settingsWindow;
+    }
+
+    createDetailWindow(cardID) {
+        if (this.mainWindow === null) {
+            throw new Error("Main window must be initialized before settings window");
+        }
+    
+        let detailWindow;  // Define detailWindow here
+    
+        let activeWindow = BrowserWindow.getFocusedWindow();
+        if (activeWindow) {
+            let { x, y } = activeWindow.getBounds();
+    
+            detailWindow = new BrowserWindow({
+                x: x + 50,
+                y: y + 50,
+                width: 800,
+                height: 600,
+                minWidth: 400,
+                minHeight: 400,
+                titleBarStyle: "hidden",
+                webPreferences: {
+                    preload: path.join(app.getAppPath(), 'packages/preload/dist/main.cjs'),
+                    scrollBounce: true
+                },
+            });
+        }
+    
+        if (!detailWindow) {
+            // Handle the case where detailWindow is not initialized
+            return;
+        }
+    
+        this.#loadEntryPoint(detailWindow, `detail.html?cardID=${cardID}`);
+    
+        detailWindow.once("ready-to-show", () => {
+            detailWindow.show();
+        });
+    
+        detailWindow.on("closed", () => {
+            detailWindow = null;
+        });
+    
+        return detailWindow;
+    }
+
+}
+
+module.exports = WindowManager
---
+++ packages/preload/src/main.js
@@ -0,0 +1,93 @@
+// preload.js
+import {contextBridge, ipcRenderer } from 'electron';
+
+contextBridge.exposeInMainWorld('utils', {
+    reloadAll: (...args) => ipcRenderer.invoke("reloadAll", ...args),
+    uploadCardEditing: (id, entry) => ipcRenderer.invoke("uploadCardEditing", id, entry),
+    markdownRender: (rawText) => ipcRenderer.invoke("markdownRender", rawText),
+    copyTextToClipboard:function(text){
+        return ipcRenderer.invoke("copyTextToClipboard", text);
+    },
+    pasteTextFromClipboard:function(){
+        return ipcRenderer.invoke("pasteTextFromClipboard");
+    },
+    platform: function(){
+        return ipcRenderer.invoke("platform");
+    },
+});
+
+contextBridge.exposeInMainWorld('pages', {
+    showCardDetail: function(cardID) {
+        return ipcRenderer.invoke("showCardDetail", cardID)
+    },
+    duplicateWindow: function(){
+        return ipcRenderer.invoke("duplicateWindow");
+    }
+});
+
+//black magic
+contextBridge.exposeInMainWorld('backendBridge', {
+    displayCardCounts: (callback) => ipcRenderer.on("displayCardCounts", (callback)),
+});
+
+(function () {
+    const paginatedDFFunc = [
+        "getCards",
+        "getCardsByTag",
+        "getTrashCards",
+        "getNoTagCards",
+        "getCardsByMiddleID",
+
+    ]
+    const fullsetDBFunc = [
+        "getLinkAtRegex",
+        "getTagRegex",
+        "removeCardFromTrash",
+        "removeCardPermanently",
+        "restoreCard",
+        "searchCards",
+        "createNewCard",
+        "getAllTags",
+        "getCardByID",
+        "moveCardToTrash",
+        "updateCardEntryByID",
+        "getCardDetails",
+        "cardIsExisted",
+        "getMaxCardID",
+        "getAllCards",
+        "searchCardsWithStyle",
+        "getCardSearchSuggestions",
+        "countDifferentCards",
+        "countTaggedCards",
+    ]
+    const needRecount = new Set([
+        "removeCardFromTrash",
+        "removeCardPermanently",
+        "moveCardToTrash",
+        "createNewCard",
+    ])
+    let backendFunctions = {};
+    paginatedDFFunc.forEach((funcName) => {
+        backendFunctions[funcName] = (...args) => {
+            localStorage.setItem('list_call', JSON.stringify({
+                'funcName': funcName,
+                'args': args,
+            }));
+            console.log(funcName, ...args);
+            return ipcRenderer.invoke(funcName, ...args)
+        };
+    })
+    fullsetDBFunc.forEach((funcName) => {
+        backendFunctions[funcName] = (...args) => {
+            let ipcResult = ipcRenderer.invoke(funcName, ...args);
+            if (needRecount.has(funcName)){
+                setTimeout(function(){
+                    //starting spell the India black magic
+                    ipcRenderer.invoke('displayCardCounts');
+                }, 200);
+            }
+            return ipcResult
+        }
+    })
+    contextBridge.exposeInMainWorld('db', backendFunctions);
+})();
