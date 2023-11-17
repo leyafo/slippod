ab650d478fc3501d0ff5838c891a074946fec3ca
leyafo <leyafo@gmail.com>
2023-11-17 11:34:22 +0800

    refresh license when reload window

--- src/main/ipc.js
+++ src/main/ipc.js
@@ -9,6 +9,22 @@ const fs = require('fs')
 const dbSchema = require('./schema.js');
 
 const licenseKey = 'license'
+let globalLicense = {}
+
+function acquireLicense(){
+    let tokenStr = db.getConfig(licenseKey);
+    if(tokenStr == ''){
+        return {}
+    }
+    let licenseObj = {}
+    try{
+        licenseObj =  JSON.parse(tokenStr)
+    }catch(error){
+        console.error(error)
+        return {}
+    }
+    return licenseObj
+}
 
 ipcMain.handle("duplicateWindow", async function(event, ...args)  {
     let newMainWindow = windowMgr.duplicateMainWindow();
@@ -54,24 +70,17 @@ ipcMain.handle("showRegisterWindow", async function(event){
 });
 
 ipcMain.handle("getLicense", async function(event){
-    let licenseToken = db.getConfig(licenseKey);
-    if(licenseToken == ''){
-        return {}
-    }
-    let licenseObj = {}
-    try{
-        licenseObj =  JSON.parse(licenseToken)
-    }catch(error){
-        licenseObj.isValid = false
-        return licenseObj 
-    }
+    globalLicense = acquireLicense()//refresh global license
+    console.log(globalLicense)
+    let licenseObj = globalLicense
     let isValid = await licenseModule.checkLicense(licenseObj)
     licenseObj.isValid = isValid
     return licenseObj
 })
 
 ipcMain.handle("reloadAll", async function(event, ...args)  {
-    windowMgr.reloadAllMainWindow()
+    globalLicense = acquireLicense()
+    return windowMgr.reloadAllMainWindow()
 });
 
 ipcMain.handle("reloadCurrentWindow", async function(event, ...args){
@@ -140,12 +149,7 @@ async function registerDBFunctions(){
         "restoreCard",
         "setConfig",
     ])
-    let license = {}
-    try{
-        license = JSON.parse(db.getConfig(licenseKey))
-    }catch(error){
-        license = {}
-    }
+    globalLicense = acquireLicense()
     const lastCreatedCard = db.getCards(0, 1)
     let lastCreatedTime = Date.now()
     if(lastCreatedCard.length != 0){
@@ -154,7 +158,7 @@ async function registerDBFunctions(){
     functionNames.forEach(function(funcName)  {
         ipcMain.handle(funcName, async function(event, ...args)  {
             if (needCheckFunctions.has(funcName)){
-                let result = await licenseModule.checkLicense(license, lastCreatedTime)
+                let result = await licenseModule.checkLicense(globalLicense, lastCreatedTime)
                 if (result == false){
                     return {"error":"license is not valid"}
                 }
