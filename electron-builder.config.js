eadc9cbba2ba4896ae2ba25a297e5697db20f654
leyafo <leyafo@gmail.com>
2023-11-27 10:04:44 +0800

    添加本地编译生成环境图标区分

--- electron-builder.config.js
+++ electron-builder.config.js
@@ -1,40 +1,49 @@
-
 module.exports = async function () {
+    let env = process.env.NODE_ENV
 
-  return {
-    directories: {
-      output: 'out',
-      buildResources: 'buildResources',
-    },
-    artifactName: "slippod.${ext}",
-    files: [
-        {
-            "from": "dist",
-            "to": "src",
+    let fileSet = "icons/"
+    if(env != undefined){
+        fileSet = {
+            "from": "icons/",
+            "to": "icons/",
+            "filter": ["**white**"]
+        }
+    }
+
+    return {
+        directories: {
+            output: 'out',
+            buildResources: 'buildResources',
         },
-        "package.json",
-        "**/node_modules/**/*"
-    ],
-    extraResources: [
-          "libsimple/",
-          "icons/",
-    ],
-    extraMetadata: {
-      version: process.env.npm_package_version,
-    },
+        artifactName: "slippod.${ext}",
+        files: [
+            {
+                "from": "dist",
+                "to": "src",
+            },
+            "package.json",
+            "**/node_modules/**/*"
+        ],
+        extraResources: [
+            "libsimple/",
+            fileSet,
+        ],
+        extraMetadata: {
+            version: process.env.npm_package_version,
+        },
 
-    // Specify linux target just for disabling snap compilation
-    linux: {
-      target: "AppImage",
-      icon: "icons/icon.png",
-    },    
-    mac: {
-      target: "dmg",
-      icon: "icons/icon.icns",
-    },
-    win: {
-      target: "nsis",
-      icon: "icons/icon.ico",
-    },
-  };
+        // Specify linux target just for disabling snap compilation
+        linux: {
+            target: "AppImage",
+            icon: (env === undefined)? "icons/icon.png":"icons/icon_white.png",
+        },
+        mac: {
+            target: "dmg",
+            icon: (env === undefined)? "icons/icon.icns":"icons/icon_white.icns",
+        },
+        win: {
+            target: "nsis",
+            icon: (env === undefined)? "icons/icon.ico":"icons/icon_white.ico",
+        },
+    };
 };
