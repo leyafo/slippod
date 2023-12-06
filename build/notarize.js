2947eb8b94c874d421895664512cb908c5c007e3
leyafo <leyafo@gmail.com>
2023-12-06 16:20:46 +0800

    add macos notarize

---
+++ build/notarize.js
@@ -0,0 +1,18 @@
+const { notarize } = require('electron-notarize');
+
+exports.default = async function notarizing(context) {
+  const { electronPlatformName, appOutDir } = context;  
+  if (electronPlatformName !== 'darwin') {
+    return;
+  }
+
+  const appName = context.packager.appInfo.productFilename;
+
+  console.log(process.env.MACOS_CERTIFICATE);
+  return await notarize({
+    appBundleId: 'com.anywherearctest.slippodtest',
+    appPath: `${appOutDir}/${appName}.app`,
+    appleId: "iosmediadev@gmail.com",
+    appleIdPassword: "SlippodDevDemo",
+  });
+};
